9c3f128e9d59e6fa5f6934e6f1724a6156c557f0
enya <ebe90@uclive.ac.nz>
2025-10-30 19:56:51 +1300

    Progression in the main container's content

--- enya-portfolio/src/MainContainer.jsx
+++ enya-portfolio/src/MainContainer.jsx
@@ -4,9 +4,21 @@ import NavBar from "./NavBar.jsx";
 function MainContainer() {
     return (
         <div className={"container-box"}>
-            <div className={"box-content"}>
+            <div className={"container-content"}>
                 <NavBar/>
-                hewwoooo
+                <div className={"box-content"}>
+                    <div className={'page-title'}>
+                        <h1>hello, i'm <span style={{color: "#7770D9"}}>enya!</span></h1>
+                    </div>
+                    <h2>second year ui/ux design student, web & app developer</h2>
+                </div>
+                <div className={"divider"}>
+                    <ul>
+                        <li>about</li>
+                        <li>works</li>
+                        <li>contact</li>
+                    </ul>
+                </div>
             </div>
         </div>
     )
