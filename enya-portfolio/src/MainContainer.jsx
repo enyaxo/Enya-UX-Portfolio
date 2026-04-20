245adaef8c1845b96b3a81dc441e35388dbb3ecd
enya <ebe90@uclive.ac.nz>
2026-04-20 14:29:45 +1200

    Nav Bar fix + minor edits

--- enya-portfolio/src/MainContainer.jsx
+++ enya-portfolio/src/MainContainer.jsx
@@ -26,23 +26,26 @@ function MainContainer() {
     return (
         <div className={"container-box"}>
             <div className={"container-content"}>
-                <NavBar/>
-                <div className={"box-content"}>
-                    <div className={'page-title'}>
-                        <h1>Hello, I'm <span>Enya!</span></h1>
+                <div style={{position:"sticky", top:"0", zIndex: 100}}><NavBar/></div>
+                <div className={"scrollable-content"}>
+                    <div className={"box-content"}>
+                        <div className={'page-title'}>
+                            <h1>Hello, I'm <span>Enya!</span></h1>
+                        </div>
+                        <h2>third year ux/ui design student, web & app developer</h2>
                     </div>
-                    <h2>Second year UI/UX design student, web & app developer</h2>
+                    <div className={"divider"}>
+                        <ul>
+                            <li onClick={() => setActivePage('about')} className={'nav-button'}>About</li>
+                            <li onClick={() => setActivePage('works')} className={'nav-button'}>Works</li>
+                            <li onClick={() => setActivePage('contact')} className={'nav-button'}>Contact</li>
+                        </ul>
+                    </div>
+                    <div className={'page-content'}>
+                        {RenderPage()}
+                    </div>
                 </div>
-                <div className={"divider"}>
-                    <ul>
-                        <li onClick={() => setActivePage('about')} className={'nav-button'}>About</li>
-                        <li onClick={() => setActivePage('works')} className={'nav-button'}>Works</li>
-                        <li onClick={() => setActivePage('contact')} className={'nav-button'}>Contact</li>
-                    </ul>
-                </div>
-                <div className={'page-content'}>
-                    {RenderPage()}
-                </div>
+
             </div>
         </div>
     )
